c6c0cf69f144881c1da569f761a673ba0d6de823
tomar-s <74296773+tomar-s@users.noreply.github.com>
2021-04-10 12:14:44 +0100

    added Waiting table

--- CreateTable.py
+++ CreateTable.py
@@ -3,12 +3,16 @@ import mysql.connector as mysql
 
 mydb = mysql.connect(
   host="localhost",
-  user="admin",
-  password="admin",
+  user="xxxx",
+  password="xxxx",
   database="mydatabase"
 )
 
 mycursor = mydb.cursor()
 
 #mycursor.execute("CREATE TABLE TRIP_DETAILS (tripId VARCHAR(255) PRIMARY KEY, owner_unique_Id VARCHAR(255), source VARCHAR(255), destination VARCHAR(255), route_taken VARCHAR(255), vehicle_number VARCHAR(255), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)")
-mycursor.execute("CREATE TABLE ROUTE_DETAILS (routeId VARCHAR(255) PRIMARY KEY, source VARCHAR(255), destination VARCHAR(255))")
+#mycursor.execute("CREATE TABLE ROUTE_DETAILS (routeId VARCHAR(255) PRIMARY KEY, source VARCHAR(255), destination VARCHAR(255))")
+mycursor.execute("CREATE TABLE WAITING_LIST(tripId VARCHAR(255) PRIMARY KEY, owner_unique_Id VARCHAR(255), source VARCHAR(255), destination VARCHAR(255), route_taken VARCHAR(255), vehicle_number VARCHAR(255), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)")
+
+
+
--- Minion.py
+++ Minion.py
@@ -6,8 +6,8 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
   host="localhost",
-  user="admin",
-  password="admin",
+  user="root",
+  password="toor",
   database="mydatabase"
 )
 
